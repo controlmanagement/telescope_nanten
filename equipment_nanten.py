30b1727d3d7e1e6845eb3f2d13c05dfd1d8c0690
Shohei <maruyama@a.phys.nagoya-u.ac.jp>
2016-02-12 18:14:16 +0900

    Update equipment_nanten.py

--- equipment_nanten.py
+++ equipment_nanten.py
@@ -10,31 +10,19 @@
 ------------------------------------------------
 [History]
 
-2015/12/16 maruyama
 
 ------------------------------------------------
 """
 
-import telescope_nanten.abs
-import telescope_nanten.m4
-import telescope_nanten.ac240
+#import telescope_nanten.abs
+#import telescope_nanten.m4
+#import telescope_nanten.ac240
 import telescope_nanten.antenna_nanten_controller
-#import pymeasure.weather
+import telescope_nanten.antenna_enc
+import telescope_nanten.dome
+import pymeasure.weather
 
 # ---------- #
-
-class antenna(object):
-    
-    def __init__(self):
-        host = '172.20.0.11'
-        port = 8003
-        self.antenna = telescope_nanten.antenna_nanten_controller.antenna_client(host, port)
-        pass
-    """
-    def drive_on(self):
-    """
-
-
 class hot_load(object):
     
     def __init__(self):
@@ -125,33 +113,84 @@ class read_status(object):
         m4_m_port = 6004
         self.l_m = telescope_nanten.abs.abs_monitor_client(opt_host, l_m_port)
         self.m4_m = telescope_nanten.m4.m4_monitor_client(opt_host, m4_m_port)        
-        self.weather = pymeasure.weather.weather_controller()
         """
+        
         ctrl_host = '172.20.0.11'
         enc_port = 8002
         antenna_port = 8004
+        domepos_port = 8006
+        dome_port = 8008
+        weather_port = 3002
         self.enc = telescope_nanten.antenna_enc.enc_monitor_client(ctrl_host, enc_port)
-        self.antenna = telescope_nanten.antenna_nanten_controller.antenna_monitor_client(ctrl_host, antenna_port)
+        self.tel = telescope_nanten.antenna_nanten_controller.antenna_monitor_client(ctrl_host, antenna_port)
+        self.dome = telescope_nanten.dome.dome_monitor_client(ctrl_host, dome_port)
+        self.weather = pymeasure.weather.weather_monitor_client(ctrl_host, weather_port)
         pass
     """
-    def get_beam(self):
+    def read_beam(self):
         load_status = self.l_m.read_pos()
         m4_status = self.m4_m.read_pos()
         return [load_status, m4_status]
-    
-    def get_weather(self):
-        ret = self.weather.get_weather()
-        return ret
     """
+    def read_weather(self):
+        ret = self.weather.read_weather()
+        return ret
     
-    def get_antenna(self):
-        enc = self.enc.read_azel()
-        target = self.antenna.read_targetazel()
+    
+    def read_antenna(self):
+        telstatus = self.tel.read_error()
+        telstatus2 = self.tel.read_status()
+        telstatus3 = self.tel.read_azel()
+        encstatus = self.enc.read_azel()
+        domestatus = self.dome.read_status()
+        domeposstatus = self.dome.read_domepos()
+        limit = ''
+
+        # limit check
+        if telstatus[4] == 0:
+            limit += 'soft limit cw-'
+        if telstatus[5] == 0:
+            limit += 'soft limit ccw-'
+        if telstatus[6] == 0:
+            limit += 'soft limit up-'
+        if telstatus[7] == 0:
+            limit += 'soft limit down-'
+        if telstatus[8] == 0:
+            limit += '1st limit cw-'
+        if telstatus[9] == 0:
+            limit += '1st limit ccw-'
+        if telstatus[10] == 0:
+            limit += '1st limit up-'
+        if telstatus[11] == 0:
+            limit += '1st limit down-'
+        if telstatus[12] == 0:
+            limit += '2nd limit cw-'
+        if telstatus[13] == 0:
+            limit += '2nd limit ccw-'
+        if telstatus[14] == 0:
+            limit += '2nd limit up-'
+        if telstatus[15] == 0:
+            limit += '2nd limit down-'
+        if telstatus[18] == 1:
+            limit += 'deviation error az-'
+        if telstatus[19] == 1:
+            limit += 'deviation error el-'
+        if telstatus[20] == 1:
+            limit += 'controller error az-'
+        if telstatus[21] == 1:
+            limit += 'controller error el-'
+        #if telstatus[22] == 1:
+            #print 'servo pack error az\n'
+        #if telstatus[23] == 1:
+            #print 'servo pack error el\n'
+        #if telstatus[24] == 0:
+            #limit += 'emergency switch'
+
         #track = self.antenna.read_track()
         #error = self.antenna.read_error()
         #velocity = self.antenna.read_v()
         #return [enc[0], enc[1], target[0], target[1], track[0], track[1], error[0], error[1], error[2], error[3], velocity[0], velocity[1]]
-        return [enc[0], enc[1], target[0], target[1]]
+        return [limit, telstatus, telstatus2, telstatus3, encstatus, domestatus, domeposstatus]
     
 
 
