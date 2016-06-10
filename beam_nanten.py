991a64852640cd5df85456e4c2a0ad25bf109c25
yukihyoto <yhyoto@gmail.com>
2016-06-10 19:24:36 +0900

    Update beam_nanten.py
    necctrlとgithubのスクリプト統合作業を行った。

--- beam_nanten.py
+++ beam_nanten.py
@@ -6,16 +6,18 @@
 [Detail Description]
 
     beam_nanten(core.controller.beam): 
-        set_r
-        set_sky
+        hot_in
+        hot_out
+        stop_hot
         m4_in
         m4_out
+        stop_m4
         get_status
 
 ------------------------------------------------
 [History]
 
-2015/10/20 maruyama
+2015/12/17 maruyama
 
 ------------------------------------------------
 """
@@ -24,22 +26,29 @@ import core.controller
 
 class beam_nanten(core.controller.beam):
     """hot,m4の制御を行う"""
+    
     def __init__(self):
         import telescope_nanten.equipment_nanten
+        
         self.load = telescope_nanten.equipment_nanten.hot_load()
         self.m4 = telescope_nanten.equipment_nanten.m4()
         pass
     
-    def set_r(self):
+    def hot_in(self):
         """hotloadをrの位置に動かす"""
         self.load.move_r()
         return
     
-    def set_sky(self):
+    def hot_out(self):
         """hotloadをskyの位置に動かす"""
         self.load.move_sky()
         return
     
+    def stop_hot(self):
+        """hotloadの動作停止"""
+        self.load.stop()
+        return
+
     def m4_in(self):
         """m4をINの位置に動かす"""
         self.m4.m4_in()
@@ -50,9 +59,13 @@ class beam_nanten(core.controller.beam):
         self.m4.m4_out()
         return
     
+    def stop_m4(self):
+        """m4の動作停止"""
+        self.m4.stop()
+        return
+
     def get_status(self):
         """現在のhotload, m4の状態を取得する"""
         l_status = self.load.get_status()
         m4_status = self.m4.get_status()
-        return {"hotload_status":l_status, "m4_status":m4_status}
-    
+        return [l_status, m4_status]
